1088ca1baf5892cac72f740922f238ca75e6699a
Sani-Mohibur <mohiburrahmansani@gmail.com>
2025-05-04 01:14:17 +0600

    Adding Map

---
+++ leaflet.js
@@ -0,0 +1,11 @@
+
+// Code from leafletjs.com
+var map = L.map('map').setView([23.8041, 90.4152], 13); // Coordinates of Dhaka 23.8041° N, 90.4152° E
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([23.8041, 90.4152]).addTo(map)
+    .bindPopup('Dhaka Bus Terminal')
+    .openPopup();
